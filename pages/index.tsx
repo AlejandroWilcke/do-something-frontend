de58bfdb5f2acd88e277f4d209177afab8f4167d
Alejandro Wilcke <aleee9208@hotmail.com>
2023-03-22 15:06:03 -0300

    fix: moved meta to NextJS _document

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,6 @@ export default function Home() {
     <>
       <Head>
         <title>Do something!</title>
-        <meta name="description" content="Do something" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
         First commit with Tailwind installed!
